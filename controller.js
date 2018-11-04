8fff68eb5e2b726bbc264af86c831cc6d5c00a2b
Nov3m63r <no.v3m63r@gmail.com>
2018-11-05 02:59:10 +0300

    I send email to myself!

--- controller.js
+++ controller.js
@@ -5,22 +5,20 @@ const nodemailer = require('nodemailer');
 nodemailer.createTestAccount((err, account) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        service: 'Gmail',
         auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
+            user: 'njsmailer@gmail.com',
+            pass: 'jsnodemail'
         }
     });
 
     // setup email data with unicode symbols
     let mailOptions = {
-        from: 'Fred Foo <foo@example.com>', // sender address
-        to: 'bar@example.com, baz@example.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello BACKEND!!!1</b>' // html body
+        from: 'Friday', // sender address
+        to: 'njsmailer@gmail.com', // list of receivers
+        subject: 'Hello from NodeJS', // Subject line
+        text: 'Hello world', // plain text body
+        html: '<b>Hello BACKEND!!!</b>' // html body
     };
 
     // send mail with defined transport object
@@ -35,4 +33,4 @@ nodemailer.createTestAccount((err, account) => {
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
-});
+})
